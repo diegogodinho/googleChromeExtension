7ae49f5322715293fac7353cc5d78930d6d82700
Diego Godinho Ferreira <diegogodinho.ferreira@yahoo.com.br>
2020-07-21 17:30:32 +0100

    second commit

--- ControllerAppointmentImmigration.js
+++ ControllerAppointmentImmigration.js
@@ -1,22 +1,27 @@
 var contollerAppointment = (function () {
-    var category, SubCategory, ConfirmGNIB, GNIBNo, GNIBExDT, UsrDeclaration, GivenName, SurName,
-        DOB, Nationality, Email,, EmailConfirm, FamAppYN,
-        PPNoYN, PPNo;
-
-
-    var category, SubCategory = All, ConfirmGNIB = Renewal, GNIBNo = 750921, GNIBExDT = 05 / 12 / 2020, UsrDeclaration tick, GivenName diego, SurName = Ferreira,
-        DOB = 27 / 08 / 1987, Nationality Brazil, Email diegogodinho.ferreira@yahoo.com.br, EmailConfirm diegogodinho.ferreira@yahoo.com.br, FamAppYN No,
-            PPNoYN Yes, PPNo YE128976
-
     function setDefaultValues() {
-
     }
 
-
-
-    var category = $('#Category');
-
-
-
-
+    function init() {
+        chrome.storage.sync.set({
+            category: 'All',
+            SubCategory: 'All',
+            ConfirmGNIB: "Renewal",
+            GNIBNo: 750921,
+            GNIBExDT: '05/12/2020',
+            GivenName: 'diego',
+            SurName: 'Ferreira',
+            DOB: '27/08/1987',
+            Nationality: 'Brazil',
+            Email: 'diegogodinho.ferreira@yahoo.com.br',
+            FamAppYN: 'No',
+            PPNoYN: 'Yes',
+            PPNo: 'YE128976'
+        }, function () {
+            console.log("All the values were submitted!");
+        });
+    }
+    return { init: init };
 })();
+
+chrome.runtime.onInstalled.addListener(contollerAppointment.init);
