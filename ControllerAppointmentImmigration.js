c206ab4ff16cf7d212c2998bd6dd845536507f77
Diego Godinho Ferreira <diegogodinho.ferreira@yahoo.com.br>
2020-07-21 16:56:25 +0100

    first commit

---
+++ ControllerAppointmentImmigration.js
@@ -0,0 +1,6 @@
+var contollerAppointment = (function () {
+    var category, SubCategory = All, ConfirmGNIB = Renewal, GNIBNo = 750921, GNIBExDT = 05 / 12 / 2020, UsrDeclaration tick, GivenName diego, SurName = Ferreira,
+        DOB = 27 / 08 / 1987, Nationality Brazil, Email diegogodinho.ferreira@yahoo.com.br, EmailConfirm diegogodinho.ferreira@yahoo.com.br, FamAppYN No, 
+        PPNoYN Yes, PPNo YE128976
+    var category = $('#Category');
+})();
